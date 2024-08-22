b5079d0beae7c45e4fc110b9066eed05136e0bcc
zhanggan <2656110902@qq.com>
2024-08-22 14:18:21 +0800

    优化

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/Zhuanz/Desktop/MyProject/ReactProject/nextjs-start/app/layout\":[]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
